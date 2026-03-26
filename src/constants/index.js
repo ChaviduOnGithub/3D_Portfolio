93da1b7677b6595735c12466f49b81f7e3bdbe1f
Nisalka Jayasinghe <jayasinghenisalka@gmail.com>
2026-03-26 20:12:58 +0530

    update

---
+++ src/constants/index.js
@@ -0,0 +1,142 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    git,
+    figma,
+    threejs,
+    images,
+    github,
+    cookease
+
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Projects",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "UI,UX Designer",
+      icon: mobile,
+    },
+    {
+      title: " Frontend Developer",
+      icon: backend,
+    },
+    {
+      title: "3D Artist",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    
+  ];
+  
+  const projects = [
+    {
+      name: "Pregassist",
+      description:
+        "An offline-capable 3D Augmented Reality (AR) simulation built in Unity. It is designed to provide immediate, zero-latency visual guidance for laypersons during critical pregnancy complications.",
+      tags: [
+        {
+          name: "dart",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "C#",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "flutter",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: images,
+      source_code_link: "https://github.com/pulasthiBuddikaGit/PregAssist",
+    },
+    {
+      name: "Cookease smart recipe app",
+      description:
+        "A cross-platform mobile and web application focusing on an intuitive User Interface (UI) for dietary tracking. It integrates the Google Cloud Vision API for image-based ingredient recognition.",
+      tags: [
+        {
+          name: "react native",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "node.js",
+          color: "green-text-gradient",
+        },
+        {
+          name: "google cloud vision",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: cookease,
+      source_code_link: "https://github.com/pulasthiBuddikaGit/CookEase",
+    },
+    {
+      description:
+        "Checkout my github for more projects ",
+    
+      image: github,
+      source_code_link: "https://github.com/NisalkaJay",
+    },
+  ];
+  
+  export { services, technologies, projects };
